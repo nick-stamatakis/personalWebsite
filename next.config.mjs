7c8accca2ea4baba3e1fafc999b9ccc18d3c0c17
Nicholas Nicos Stamatakis <116750704+nick-stamatakis@users.noreply.github.com>
2025-02-16 18:43:26 -0500

    Fix GitHub Pages image paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
     output: 'export',
     basePath: isProd ? '/personalWebsite' : '',
     assetPrefix: isProd ? '/personalWebsite/' : '',
+    images: {
+        unoptimized: true, // Required for static export to GitHub Pages
+    },
 };
