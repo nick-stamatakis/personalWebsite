1533b826d29385f0fbd2138eefa163a1c1f5e01f
Nicholas Stamatakis <nick@westpalmtestprep.com>
2025-09-27 10:57:58 -0400

    fix deploy eror

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/personalWebsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personalWebsite' : '',
 };
 
 export default nextConfig;
